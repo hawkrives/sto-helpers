bb7e47942bc5a033fd21de0851bf7788acd17c54
Hawken Rives <hawkrives@gmail.com>
2015-01-11 21:05:12 -0600

    Import only needed things from lodash for zipToObjectWithArrays

--- src/zipToObjectWithArrays.js
+++ src/zipToObjectWithArrays.js
@@ -1,9 +1,9 @@
-import _ from 'lodash'
+import {reduce, zip, has} from 'lodash'
 
 let zipToObjectWithArrays = (keys, vals) => {
-	let arr = _.zip(keys, vals)
-	return _(arr).reduce((obj, propKey) => {
-		if (_.has(obj, propKey[0]))
+	let arr = zip(keys, vals)
+	return reduce(arr, (obj, propKey) => {
+		if (has(obj, propKey[0]))
 			obj[propKey[0]].push(propKey[1])
 		else
 			obj[propKey[0]] = [propKey[1]]
