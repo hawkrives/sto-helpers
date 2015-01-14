a90372ea0abe51daf451764ba42e2184c0f1d581
Hawken Rives <hawkrives@gmail.com>
2015-01-13 18:09:42 -0600

    Add flag for whether or not to include the section in the output

--- src/splitDeptNum.js
+++ src/splitDeptNum.js
@@ -5,17 +5,23 @@ import deptNumRegex from './deptNumRegex'
  * like {depts: ['AS', 'RE'], num: 230, sect: 'A'}.
  *
  * @param {String} deptNumString
+ * @param {Boolean} includeSection
  * @returns {Object}
  */
-function splitDeptNum(deptNumString) {
+function splitDeptNum(deptNumString, includeSection=false) {
 	// "AS/RE 230A" -> ["AS/RE 230A", "AS/RE", "AS", "RE", "230", "A"]
 	// -> {depts: ['AS', 'RE'], num: 230}
 	let matches = deptNumRegex.exec(deptNumString)
 
-	return {
+	let deptNum = {
 		depts: matches[1].indexOf('/') !== -1 ? [matches[2], matches[3]] : [matches[1]],
 		num: parseInt(matches[4], 10),
 	}
+
+	if (includeSection && matches.length >= 6 && matches[5])
+		deptNum.sect = matches[5]
+
+	return deptNum
 }
 
 export default splitDeptNum
