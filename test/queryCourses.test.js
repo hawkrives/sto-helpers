9cb4f5b89a7cc72c0b5ab072f750c967f2920f63
Hawken Rives <hawkrives@gmail.com>
2015-01-14 23:02:52 -0600

    Added a test to queryCourses

--- test/queryCourses.test.js
+++ test/queryCourses.test.js
@@ -9,4 +9,13 @@ describe('queryCourses', () => {
 		]
 		expect(queryCourses(query, courses)).to.eql(courses)
 	})
+
+	it('properly handles a list of five years', () => {
+		let query = 'year: $OR year: 2010 year: 2011 year: 2012 year: 2013 year: 2014'
+		let courses = [
+			{depts: ['AMCON'], year: 2013},
+			{depts: ['ASIAN'], year: 2012},
+		]
+		expect(queryCourses(query, courses)).to.eql(courses)
+	})
 })
