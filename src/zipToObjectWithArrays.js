9b26156f389cd7f17c8a413acc8c28e2e80c43e4
Hawken Rives <hawkrives@gmail.com>
2015-01-26 15:59:48 -0600

    Use array destructuring in zipToObjectWithArrays

--- src/zipToObjectWithArrays.js
+++ src/zipToObjectWithArrays.js
@@ -2,11 +2,12 @@ import {reduce, zip, has} from 'lodash'
 
 let zipToObjectWithArrays = (keys, vals) => {
 	let arr = zip(keys, vals)
-	return reduce(arr, (obj, propKey) => {
-		if (has(obj, propKey[0]))
-			obj[propKey[0]].push(propKey[1])
+
+	return reduce(arr, (obj, [key, val]) => {
+		if (has(obj, key))
+			obj[key].push(val)
 		else
-			obj[propKey[0]] = [propKey[1]]
+			obj[key] = [val]
 
 		return obj
 	}, {})
