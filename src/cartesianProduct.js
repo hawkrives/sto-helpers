1067b96646b1fabdb287280bb74f6187f195dcc3
Hawken Rives <hawkrives@gmail.com>
2015-01-26 11:07:24 -0600

    _.flatten is true by default

--- src/cartesianProduct.js
+++ src/cartesianProduct.js
@@ -4,7 +4,7 @@ function cartesianProductOf(...args) {
 	return reduce([...args], (a, b) => {
 		return flatten(map(a, (x) => {
 			return map(b, (y) => x.concat([y]))
-		}), true)
+		}))
 	}, [ [] ])
 }
 
