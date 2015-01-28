8f3787ff971e94651a8d784a2ae7d24b5ac0de2f
Hawken Rives <hawkrives@gmail.com>
2015-01-27 23:50:07 -0600

    Updated combinations test
    pulled in the tests from the package

--- test/combinations.test.js
+++ test/combinations.test.js
@@ -1,22 +1,42 @@
 // test/combinations.test.js
 import combinations from '../src/combinations'
 
+function collect(comb) {
+	let items = []
+	for (let item of comb)
+		items.push(item)
+	return items
+}
+
 describe('combinations', () => {
-	it('finds all combinations of an array', () => {
-		expect(combinations([1, 2], 2)).to.eql([[1, 2]])
+	it('should be a function and return iterator', () => {
+		expect(combinations).to.be.a.Function
+		expect(combinations()).to.be.an.Object
+		expect(combinations()).to.have.property('next')
+		expect(combinations()).to.be.a.Function
+		expect(combinations().next()).to.be.an.Object
+		expect(combinations().next()).to.have.property('done')
+	})
 
-		expect(combinations([1, 2, 3], 1)).to.eql([[1], [2], [3]])
+	it('should be iterable ', () => {
+		var comb = combinations(['a', 'b'], 2)
+		for (var item of comb) {
+			expect(item).to.be.an.Array
+			expect(item).to.eql(['a', 'b'])
+		}
+	})
 
-		expect(combinations([1, 2, 3], 2)).to.eql([[1, 2], [1, 3], [2, 3]])
+	it('should return combinations', () => {
+		var comb = combinations(['a', 'b', 'c'], 2)
+		expect(collect(comb)).to.eql([['a', 'b'], ['a', 'c'], ['b', 'c']])
+		expect(collect(combinations(new Array(7), 5))).to.have.length(21)
+	})
 
-		expect(combinations([1, 2, 3], 3)).to.eql([[1, 2, 3]])
-
-		expect(combinations([1, 2, 3], 4)).to.eql([])
-
-		expect(combinations([1, 2, 3], 0)).to.eql([])
-
-		expect(combinations([1, 2, 3], -1)).to.eql([])
-
-		expect(combinations([], 0)).to.eql([])
+	it('should not return wrong combinations', () => {
+		expect(combinations(['a'], 0).next()).to.eql({done: true, value: undefined})
+		expect(combinations(['a'], 2).next()).to.eql({done: true, value: undefined})
+		expect(combinations(['a'], -1).next()).to.eql({done: true, value: undefined})
+		expect(combinations([], 1).next()).to.eql({done: true, value: undefined})
+		expect(combinations([], 0).next()).to.eql({done: true, value: undefined})
 	})
 })
